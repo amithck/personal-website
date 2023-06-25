feb17916f4ab0352ac88b360ee5cd95c9114d0a8
amithck <amith.kowshik@gmail.com>
2023-06-25 22:18:37 +0530

    temporary change with lab manual

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Resume from '../../assets/Resume.pdf'
+import Manual from '../../assets/manual.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
         <a href={Resume} download className='btn'>Download Resume</a>
         <a href="#contact" className='btn btn-primary'>Contact Me</a>
+        <a href={Manual} download className='btn'>Lab Manual</a>
     </div>
   )
 }
