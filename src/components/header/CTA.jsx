ae4ce4a1d5dac4cd54f255a17a851c6089fd4b3a
Amith C Kowshik <87654235+amithck@users.noreply.github.com>
2023-07-05 16:27:30 +0530

    removed manual

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -7,7 +7,6 @@ const CTA = () => {
     <div className='cta'>
         <a href={Resume} download className='btn'>Download Resume</a>
         <a href="#contact" className='btn btn-primary'>Contact Me</a>
-        <a href={Manual} download className='btn'>Lab Manual</a>
     </div>
   )
 }
